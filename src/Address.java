0352bdaa7e7fa066e7938ff58e9a94305455c85f
Agata Sudol <agatasudol@o2.pl>
2020-05-05 19:45:09 +0200

    First commit

---
+++ src/Address.java
@@ -0,0 +1,14 @@
+        import java.util.Scanner;
+
+        class Address {
+            public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Enter your address: ");
+            scanner = new Scanner(System.in);
+            String address = scanner.nextLine();
+            System.out.println("You live at: " + address);
+        }
+
+}
